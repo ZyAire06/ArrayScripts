b6abceec2e82184719df36340ec428c6a6133244
Zy'Aire McCreary-Scott <zyaire.mccrearyscott@gmail.com>
2022-11-02 11:23:32 -0400

    add avg_scores add variables and function with a loop

---
+++ avg_scores.js
@@ -0,0 +1,19 @@
+let myScores = [92, 98, 84, 76, 89, 99, 100];
+console.log(myScores);
+
+let yourScores = [82, 98, 94, 88, 92, 100, 100];
+
+let sum = 0; 
+function getAverage(myScores) {
+     let numMyScores = myScores.length;
+    for(let i=0; i < numMyScores; i++){
+        console.log(`i is ${i}`);
+         
+        
+       
+    }
+
+    
+}
+
+getAverage(myScores);
