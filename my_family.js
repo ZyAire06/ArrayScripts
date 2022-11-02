84c71778f40753e07a5078cb39ddf7c046ff67f6
Zy'Aire McCreary-Scott <zyaire.mccrearyscott@gmail.com>
2022-11-02 10:56:52 -0400

    add my_family.js and console-page.html just add array and for loop

---
+++ my_family.js
@@ -0,0 +1,5 @@
+let family =  ['Dawn', 'Patricia', 'Patrice', 'Dwayne'];
+let numFamily = family.length;
+for(let i= 0; i< numFamily; i++){
+    console.log(`This is my some of my family ${family}`);
+}
