5b71ddb7cffe9c77b77eb6e73e0a367c923022ae
Zy'Aire McCreary-Scott <zyaire.mccrearyscott@gmail.com>
2022-11-03 09:36:34 -0400

    chaning avg equation

--- avg_scores.js
+++ avg_scores.js
@@ -3,17 +3,31 @@ console.log(myScores);
 
 let yourScores = [82, 98, 94, 88, 92, 100, 100];
 
-let sum = 0; 
-function getAverage(myScores) {
-     let numMyScores = myScores.length;
-    for(let i=0; i < numMyScores; i++){
-        console.log(`i is ${i}`);
-        let avg =(sum/numMyScores).toFixed(2);
-        
-       return avg;
-    }
+let sum = 0;
 
+// function getMyAverage(myScores) {
+//      let numMyScores = myScores.length;
+
+//     for(let i=0; i < numMyScores; i++){
+//         // console.log(`i is ${i}`);
+//         Scores.forEach((num) => { sum += num});
+
+//         let avg = (sum/numMyScores).toFixed(2);
+
+ 
+     
+//     }
+//     return avg;
     
-}
+// }
 
-getAverage(myScores);
+// getMyAverage(myScores);
+
+function geyYourAverage(yourScores) {
+    let numYourScores = yourScores.length;
+    for(let i=0; i<numYourScores; i++){
+    let avgs = (sum/numYourScores).toFixed(2)
+    };
+    return avgs;
+}
+geyYourAverage(yourScores);
